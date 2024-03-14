a8df9d85d3eff741be76dfbd88e9be83fb4a72f8
Sidnei <sidneisonic@hotmail.com>
2024-03-14 20:47:59 -0300

    aula 51 usando o CSS module

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,11 +1,15 @@
 // import styles from "./styles"
-import "./Card.css"
+// import "./Card.css"
+import { CardTitle } from "../CardTitle"
+import styles from "./Card.module.css"
 
 export const Card = () => {
     return (
         // <div style={styles.container}>
-        <div id="container">
+        // <div id="container">
+        <div className={styles.container}>
             <h1>Card</h1>
+            <CardTitle />
         </div>
     )
 }
---
+++ src/components/CardTitle/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./CardTitle.module.css";
+
+export const CardTitle = () => {
+    return (
+        <div className={styles.container}>
+            <h1>Card Title</h1>
+        </div>
+    )
+}
