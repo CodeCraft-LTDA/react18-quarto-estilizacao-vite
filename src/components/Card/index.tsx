2565a0399953f64cbf540a7fab25655e51c1a2d9
Sidnei <sidneisonic@hotmail.com>
2024-03-14 20:36:30 -0300

    aula 49 como organizar o inlien style

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles"
+
+export const Card = () => {
+    return (
+        <div style={styles.container}>
+            <h1>Card</h1>
+        </div>
+    )
+}
---
+++ src/components/Card/styles.ts
@@ -0,0 +1,9 @@
+import { CSSProperties } from "react";
+
+const container: CSSProperties = {
+    backgroundColor: "red", border: "1px solid red", width: 400, height: 400, color:"green"
+}
+
+export default {
+    container
+}
