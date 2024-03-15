4efe929f2323b6f50029df7b3bee9161a4cdddca
Sidnei <sidneisonic@hotmail.com>
2024-03-14 21:46:50 -0300

    aula 56 styled components animacoes

--- src/components/Input/Input.ts
+++ src/components/Input/Input.ts
@@ -1,4 +1,13 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
+
+const rotate = keyframes`
+    from {
+        transform: rotate(0deg) scale(1);
+    }
+    to {
+        transform: rotate(360deg) scale(3);
+    }
+`
 
 export const Input = styled.input`
     width: 100%;
@@ -9,6 +18,7 @@ export const Input = styled.input`
     border: 1px solid #ccc;
     outline: none;
     box-sizing: border-box;
+    animation: ${rotate} 2s alternate-reverse infinite;
     transition: 0.3s;
     &:focus {
         border: 1px solid red;
